2d8ca4583b97402275d36d2235a3b644b95edcf4
Evelyn Sionov <evesionov@gmail.com>
2023-03-22 19:07:31 -0400

    3/22/23 final home push

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+    private int x;
+    private int y;
+    private String label;
+
+    public Point(String label, int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.label = label;
+    }
+
+    public boolean equals(Point point2) {
+        if (point2 == null) {
+            return false;
+        }
+
+        if (x == point2.x && y == point2.y) {
+            return true;
+        }
+        return false;
+    }
+
+    public String toString() {
+         return label + " is at x = " + x + ", y = " + y;
+    }
 }
--- src/ThreeDPoint.java
+++ src/ThreeDPoint.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class ThreeDPoint {
+public class ThreeDPoint extends Point {
+    private int z;
+
+    public ThreeDPoint(String label, int x, int y, int z) {
+        super(label, x, y);
+        this.z = z;
+    }
+
+    public boolean equals(Point point2) {
+        if (!(point2 instanceof ThreeDPoint)) {
+            return false;
+        }
+
+        ThreeDPoint newObj = (ThreeDPoint) point2;
+
+        if (super.equals(newObj) && z == newObj.z) {
+            return true;
+        }
+        return false;
+    }
 }
